9da9bfe9a04db74d470b00723faf204b4631a0fc
subhra <subhra@kali>
2025-06-23 17:55:25 +0530

    start command

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 var fetchuser = require("../middleware/fetchuser");
 
-const JWT_SECRET = "imsubhranil";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 //Route 1: Create a user using: POST "/api/auth/createuser". No login required
 router.post(
